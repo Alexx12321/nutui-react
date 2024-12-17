bec5d7924ebd62a70272e8398a695f6cdf16deac
Alex.hxy <1872591453@qq.com>
2024-12-17 20:11:02 +0800

    chore: add unit test rate

--- src/packages/sidebar/_test_/sidebar.spec.tsx
+++ src/packages/sidebar/_test_/sidebar.spec.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import { render } from '@testing-library/react'
+import { fireEvent, render } from '@testing-library/react'
 import '@testing-library/jest-dom'
 import { SideBar } from '../sidebar'
 
@@ -17,4 +17,35 @@ test('should render defaultValue correctly', async () => {
   )
   const item = container.querySelectorAll('.nut-sidebar-titles-item')[0]
   expect(item).toHaveClass('nut-sidebar-titles-item-active')
+})
+
+test('should choose and scroll to the right option', async () => {
+  const onChange = vi.fn()
+  const { container } = render(
+    <SideBar style={{ height: 300 }} value="0" onChange={onChange}>
+      {list.map((item) => (
+        <SideBar.Item key={item} title={`Opt ${item + 1}`}>
+          Content {item + 1}
+        </SideBar.Item>
+      ))}
+    </SideBar>
+  )
+  const items = container.querySelectorAll('.nut-sidebar-titles-item')
+  fireEvent.click(items[1])
+  expect(onChange).toHaveBeenCalledWith(1)
+})
+test('disabled option', async () => {
+  const onChange = vi.fn()
+  const { container } = render(
+    <SideBar style={{ height: 300 }} value="0" onChange={onChange}>
+      {list.map((item) => (
+        <SideBar.Item key={item} title={`Opt ${item + 1}`} disabled>
+          Content {item + 1}
+        </SideBar.Item>
+      ))}
+    </SideBar>
+  )
+  const items = container.querySelectorAll('.nut-sidebar-titles-item')
+  fireEvent.click(items[1])
+  expect(onChange).not.toHaveBeenCalled()
 })
